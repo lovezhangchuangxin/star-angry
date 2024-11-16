09cba8ccb802772cad9723e921f57d68e94762da
Jason <Jason>
2024-11-17 06:41:01 +0800

    fix: element 暗黑风格

--- packages/game/src/main.ts
+++ packages/game/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './styles/main.less'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 
